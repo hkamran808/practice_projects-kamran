707bdd08990b50f757baab7c4d9d1b36b7604b35
Kamran Huseynli <k.huseynli@ufaz.az>
2025-11-09 02:35:49 +0400

    Adding last methods to modify the model

--- price_predictor.py
+++ price_predictor.py
@@ -16,18 +16,28 @@ ln = LinearRegression()
 X = df[["Size_sqft", "Bedrooms", "Bathrooms", "Age"]]
 Y = df["Price"]
 ln.fit(X, Y)
-print(ln.score(X, Y))  #getting first useful data (r^2 / determination of correlation)
+print(ln.score(X, Y))  #getting first useful data (r^2 / determination of correlation with plain model)
 
 # working with train test split to improve model
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 rr = Ridge(alpha=1.0)
 rr.fit(X_train, Y_train)
-print(rr.score(X_test, Y_test))  #getting second useful data (r^2 / determination of correlation)
+print("Ridge Regression R²: ", rr.score(X_test, Y_test))  #getting second useful data (r^2 / determination of correlation with ridge regression)
+
+from sklearn.linear_model import Lasso
+ll = Lasso(alpha=1.0)
+ll.fit(X_train, Y_train)
+print("Lasso Regression R²: ", ll.score(X_test, Y_test))  #getting third useful data (r^2 / determination of correlation with lasso regression)
+
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(ln, X, Y, cv=5)
+print("Cross-validation scores: ", scores)
+import numpy as np
+print("Average cross-validation score: ", np.mean(scores))  #getting fourth useful data (r^2 / determination of correlation with cross validation)
 
 # features of our model formula: intercept and all coefficients
-print("Intercept of model's formula: ", ln.intercept_)
-#print(ln.coef_)
+print("Intercept of model's formula: ", ln.intercept_)  #print(ln.coef_)
 for attribute, coef in zip(X.columns, ln.coef_):
     print(f"{attribute}: {coef:.2f}")
 
@@ -40,7 +50,7 @@ plt.ylabel("Predicted")
 plt.title("Accuracy of the model")
 plt.show()
 """
-#adding MAE and RMSE
+#adding MAE and RMSE to check accuracy between actual and predicted prices
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 mae = mean_absolute_error(Y_test, Y_pred)
@@ -69,5 +79,3 @@ age = int(input("Enter house age: "))
 new_house = [[size_sqft, bedrooms, bathrooms, age]]
 predicted_price = ln.predict(new_house)
 print(f"Predicted price for the new house: ${predicted_price[0]:.2f}")
-
-# LASSO PARAMETER & CROSS-VALIDATION TO BE ADDED!!!
