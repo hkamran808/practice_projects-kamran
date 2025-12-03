aad9970b41cc3e5c64e733c4f79f8f9263ad0bb6
Kamran Huseynli <k.huseynli@ufaz.az>
2025-12-04 03:58:49 +0400

    automator improved with interactive mode

--- automator_sqlite.py
+++ automator_sqlite.py
@@ -1,8 +1,11 @@
 import sqlite3
+from tabulate import tabulate
+from datetime import datetime
 
 conn = sqlite3.connect("database1.db")
 cur = conn.cursor()
 
+"""
 with open("queries.sql", "r") as file:
     sql_commands = file.read().split(";")
 
@@ -26,6 +29,35 @@ for command in sql_commands:
             print("command executed successfully!")
     except Exception as e:
         print(f"exception (error) occurred: {e}")
+"""
+
+# Interactive mode below:
+print("Database connected. Enter your SQL commands! or quit!")
+
+while True:
+    command = input("SQL>>").strip()
+    if command.lower() in ("exit", "quit", "leave"):
+        print("You quitted, goodbye!")
+        break
+    if command == "" or command.startswith("--"):
+        continue
+
+    try:
+        cur.execute(command)
+    except Exception as e:
+        print("SQL error", e)
+
+    first_cmd = command.split()[0].lower()
+    if first_cmd == "select":
+        rows = cur.fetchall()
+        print("number of rows fetched:", len(rows))
+        if len(rows) > 0:
+            print("here they are: ")
+        for row in rows:
+            print(row)
+    else:
+        conn.commit()
+        print("Executed! Rows that are affected: ", cur.rowcount)
 
 conn.close()
 print("all commands executed, connection is closed!")
