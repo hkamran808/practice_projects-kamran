8cc81dda6175fa314876222ce6d59592f11e0951
Kamran Huseynli <k.huseynli@ufaz.az>
2025-12-09 03:00:50 +0400

    SQLite Automator: Menu and new functions added to make project more professional and clean

--- automator_sqlite.py
+++ automator_sqlite.py
@@ -9,68 +9,89 @@ def log(message):
 
 conn = sqlite3.connect("database1.db")
 cur = conn.cursor()
+print("Database connected: ")
 
-"""
-with open("queries.sql", "r") as file:
-    sql_commands = file.read().split(";")
+# Interactive mode with UI below:
+def menu():
+    print("--- My SQLite Automator! ---")
+    print("1. Show all users")
+    print("2. Add a new user")
+    print("3. Update user age")
+    print("4. Delete user")
+    print("5. Search user by name")
+    print("0. Quit")
 
-for command in sql_commands:
-    command = command.strip()
-    if command == "":
-        continue
-    print(f"current command: {command}")
+def show_users():
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    if len(rows) == 0:
+        print("no data found!")
+    if len(rows) > 0:
+        print("users => ")
+        print(tabulate(rows, headers=columns, tablefmt="fancy_grid"))
+        print(f"{len(rows)} rows x {len(columns)} columns")
 
-    try:
-        cur.execute(command)
-        if command.lower().startswith("select"):
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
-            print(row)
-            print(f"number of rows: {len(rows)}")
+def add_user():
+    name = input("Enter name of user: ")
+    age = input("Enter age of user: ")
 
-        else:
-            conn.commit()
-            print("command executed successfully!")
-    except Exception as e:
-        print(f"exception (error) occurred: {e}")
-"""
+    cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))
+    conn.commit()
+    print("user added succesfully!")
 
-# Interactive mode below:
-print("Database connected. Enter your SQL commands! or quit!")
+def update_user():
+    id = int(input("Enter ID of user: "))
+    new_age = input("Enter updated age of user: ")
+    cur.execute("UPDATE users SET age=? WHERE id=? VALUES()", (new_age, id))
+    conn.commit()
+    print("user's age updated succesfully!")
+
+def delete_user():
+    user_id = int(input("Enter ID of user: "))
+    cur.execute("DELETE FROM users WHERE id=?", (user_id,))
+    conn.commit()
+    print("user deleted succesfully!")
+
+def search_user_byName():
+    searched_name = input("Enter name to search: ")
+    cur.execute("SELECT * FROM users WHERE name LIKE ?", ('%'+searched_name+'%',))
+    rows = cur.fetchall()
+
+    if len(rows) == 0:
+        print("No user found!")
+        return
+    
+    columns = [desc[0] for desc in cur.description]
+    print(tabulate(rows, headers=columns, tablefmt="fancy_grid"))
+    print(f"({len(rows)} rows) x {len(columns)} columns")
 
 while True:
-    command = input("SQL>>").strip()
-    if command.lower() in ("exit", "quit", "leave"):
-        print("You quitted, goodbye!")
-        break
-    if command == "" or command.startswith("--"):
-        continue
-
+    menu()
+    choice = input("OPTION>> ").strip()
+    match choice:
+        case "1":
+            show_users() 
+        case "2":
+            add_user()
+        case "3":
+            update_user()
+        case "4":
+            delete_user()
+        case "5":
+            search_user_byName()
+        case "0" | "quit" | "exit" | "leave":
+            print("Quitting...")
+            break
+        
+    """
     try:
         cur.execute(command)
     except Exception as e:
         print("SQL error", e)
         log(f"ERROR: {command} | reason: {e}")
         continue
-
-    first_cmd = command.split()[0].lower()
-    if first_cmd == "select":
-        rows = cur.fetchall()
-        columns = [desc[0] for desc in cur.description]
-        print("number of rows fetched:", len(rows))
-        if len(rows) == 0:
-            print("no data found!")
-        if len(rows) > 0:
-            print("result table => ")
-            print(tabulate(rows, headers=columns, tablefmt="fancy_grid"))
-            print(f"Displayed: {len(rows)} rows x {len(columns)} columns")
-        #for row in rows:
-        #    print(row)
-    else:
-        conn.commit()
-        log(f"SUCCESS: {command} | rows affected: {cur.rowcount}")
-        #print("Executed! Rows that are affected: ", cur.rowcount)
-
+    """
+    
 conn.close()
-print("all commands executed, connection is closed!")
+print("connection is closed!")
