acbf3d9c9e3f1d37343a3da687a0e2e7aab36727
Kamran Huseynli <k.huseynli@ufaz.az>
2025-12-20 19:53:45 +0400

    Student Performance Analytics & Grade Prediction System: 4 new classifier method and summary of their accuracies' comparison added, with slight changes needed

--- student_analyticsANDgrade_prediction.py
+++ student_analyticsANDgrade_prediction.py
@@ -65,3 +65,98 @@ mean_treatment = df_clean[df_clean['test_preparation']=='completed']['avg_score'
 
 uplift = mean_treatment - mean_control
 print(f"mean_treatment ({mean_treatment}) - mean_control ({mean_control}) = uplift ({uplift})")
+
+#1. Preparing Data
+from sklearn.model_selection import train_test_split
+features = [
+    "study_hours",
+    "sleep_hours",
+    "math_score",
+    "reading_score",
+    "writing_score"
+]
+x = df[features]
+y = df["pass_fail"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+#2. scaling to avoid imbalance
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#3. Logistic Regressor Model
+from sklearn.linear_model import LogisticRegression
+LR_model = LogisticRegression()
+LR_model.fit(X_train_scaled, Y_train)
+prediction_LR = LR_model.predict(X_test_scaled) #0-fail, 1-pass
+
+#Accuracy and Report
+from sklearn.metrics import accuracy_score, classification_report
+print("Accuracy:", accuracy_score(Y_test, prediction_LR))
+print(classification_report(Y_test, prediction_LR))
+
+#4. Interpreting LR Model (weights of features=> '+' is increase | '-' is decrease of probability of class 1 (pass))
+for feature, coef in zip(features, LR_model.coef_[0]):
+    print(feature, coef)
+"""
+ps: we use [0] to get into array, because coef_ is 2D
+coef_[0] represents how features affect the probability of the positive class (label 1)
+"""
+
+#5. K-Nearest-Neighbors (w/ accuracy & report)
+from sklearn.neighbors import KNeighborsClassifier
+for i in [1, 3, 5, 10, 20]:
+    knn_model = KNeighborsClassifier(n_neighbors=i)
+    knn_model.fit(X_train_scaled, Y_train)
+    prediction_KNN = knn_model.predict(X_test_scaled)
+    prediction_probabilities_KNN = knn_model.predict_proba(X_test_scaled)
+
+    print(f"Accuracy with {i} nearest neighbors: ", {accuracy_score(Y_test, prediction_KNN)})
+    print(f"Classification Report for {i} nearest neighbors: ", {classification_report(Y_test, prediction_KNN)})
+
+#6. Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+NB_model = GaussianNB()
+NB_model.fit(X_train_scaled, Y_train)
+prediction_NB = NB_model.predict(X_test_scaled)
+prediction_probabilities_NB = NB_model.predict_proba(X_test_scaled)
+
+from sklearn.metrics import accuracy_score, classification_report
+print("Accuracy (Naive Bayes):", accuracy_score(Y_test, prediction_NB))
+print("Classification Report (Naive Bayes):", classification_report(Y_test, prediction_NB))
+
+#Interpretation of NB model
+print("Mean per class: ", NB_model.theta_) # mean per feature per class
+print("Variance per class: ", NB_model.var_) # variance per feature per class
+
+#7. K-Means
+from sklearn.cluster import KMeans
+
+KM_model = KMeans(n_clusters=2, random_state=1) # number of classes (pass/fail)
+KM_model.fit(X_train_scaled)
+
+cluster_labels = KM_model.labels_  # Get cluster labels assigned by K-Means
+import numpy as np
+from scipy.stats import mode
+mapped_labels = np.zeros_like(cluster_labels) # Map clusters to actual classes (pass/fail)
+
+for cluster in range(2):  # 2 clusters, and for each cluster, assign the most frequent label inside it
+    true_false = (cluster_labels == cluster)
+    mapped_labels[true_false] = mode(Y_train[true_false])[0]
+
+# Accuracy and Report
+print("Accuracy of K-Means: ", accuracy_score(Y_train, mapped_labels))
+print("Classification Report of K-Means: ", classification_report(Y_train, mapped_labels))
+
+#8. SUMMARY
+model_names = ["Logistic Regression", "Naive Bayes", "K-Means"]
+accuracies = [
+    accuracy_score(Y_test, prediction_LR),
+    accuracy_score(Y_test, prediction_KNN),
+    accuracy_score(Y_test, prediction_NB),
+    accuracy_score(Y_test, mapped_labels)
+]
+for name, acc in zip(model_names, accuracies):
+    print(f"{name}'s accuracy: {acc}")
