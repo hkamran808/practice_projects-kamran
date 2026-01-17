ef8b1600ab65960f7179c0238d3ddaaaef25ebb3
Kamran Huseynli <k.huseynli@ufaz.az>
2026-01-17 05:02:26 +0400

    Feature Engineering & Validation: Errors made MAE higher detected => heteroscedasticity, linear model uneffective, errors grows with target. Target transformation to be done!

--- Feature_Engineering_and_Validation.py
+++ Feature_Engineering_and_Validation.py
@@ -81,3 +81,17 @@ print("Improvised modeling complete and evaluated!")
 
 #Mean Absolute Error before feature engineering: 105891.87533810796
 #Mean Absolute Error after feature engineering: 113909.54201972325
+
+import matplotlib.pyplot as plt
+scatter_plot = plt.scatter(Y_test, predictions, alpha=0.5)
+plt.xlabel("Actual SalePrice")
+plt.ylabel("Predicted SalePrice")
+plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'r--')  # Diagonal line
+plt.show()
+
+residual = Y_test - predictions
+residual_plot = plt.scatter(predictions, residual, alpha=0.5)
+plt.xlabel("Predicted SalePrice")
+plt.ylabel("Residuals")
+plt.axhline(y=0, color='r', linestyle='--')
+plt.show()
